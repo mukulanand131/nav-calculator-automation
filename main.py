42cb1975c7c48bff235e677c04f42b8a3b6d1b98
Mukul Anand <mukulanand131@gmail.com>
2025-07-31 16:13:45 +0530

    Update main.py
    Running multiple url and handling their data in sheet

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from MutualFundAnalyzer import MutualFundAnalyzer
 
 if __name__ == "__main__":
-    analyzer = MutualFundAnalyzer("https://groww.in/mutual-funds/sbi-psu-fund-direct-growth")
-    analyzer.run_analysis()
+    urls = [
+        'https://groww.in/mutual-funds/sbi-psu-fund-direct-growth',
+        'https://groww.in/mutual-funds/icici-prudential-value-discovery-fund-direct-growth',
+        # Add more URLs as needed
+    ]
+
+    for url in urls:
+        analyzer = MutualFundAnalyzer(url, base_workers=5)
+        analyzer.run_analysis()
